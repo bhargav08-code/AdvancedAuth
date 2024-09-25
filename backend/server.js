6471d6ace9954cdd7473d416a8c6027cfb64ce33
bhargav08-code <bhargavgarge1828@gmail.com>
2024-09-26 02:56:38 +0530

    Done with signUI and functionality

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./Routes/auth.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import FloatingShape from "./components/FloatingShape";
-
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
 export default function App() {
   return (
     <>
@@ -7,7 +10,18 @@ export default function App() {
         className="min-h-screen bg-gradient-to-br
     from-gray-900 via-purple-900 to-red-600 flex items-center justify-center relative overflow-hidden"
       >
-        <FloatingShape />
+        <FloatingShape
+          color="bg-purple-500"
+          size="w-64 h-64"
+          top="-5%"
+          left="10%"
+          delay={0}
+        />
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/verify-email" element={<EmailVerificationPage />} />
+        </Routes>
       </div>
     </>
   );
